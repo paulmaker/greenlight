918fe436f817a589133b32cec5398c0e875e6ea4
root <root@ip-172-31-6-181.eu-west-1.compute.internal>
2020-11-05 16:41:34 +0000

    First Commit All modifications

--- config/application.rb
+++ config/application.rb
@@ -168,5 +168,8 @@ module Greenlight
 
     # Default admin password
     config.admin_password_default = ENV['ADMIN_PASSWORD'] || 'administrator'
+
+    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOWALL"
+    
   end
 end
---
+++ terms.md
@@ -0,0 +1,1 @@
+This is the T's & C's page
